0bf06f3b5155e20213f641790daa3b8fb8e6e805
prachisuvks <trishssyadav@gmail.com>
2026-02-17 17:50:09 -0600

    have an infinite loop I need to fix

--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -1,28 +1,35 @@
-  import java.io.*;
+import java.io.*;
 import java.util.Scanner;
-public class SpotifyTester {
- 
 
 public class SpotifyTester {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner inF = new Scanner(new File("spotify_unique_25.txt"));
-        System.out.println("Title\t\t\t\t Artist\t\t\t\t Album\t\t\t\t\t\t\t\t\t Duration \t\tYear \t\t\tGenre");
-        while (inF.hasNextLine()) {
-            String line = inF.nextLine();
-            String arr[] = line.split(",");
-            String name = arr[0];
-            String artist = arr[1];
-            String album = arr[2];
-            int duration = Integer.parseInt(arr[3]);
-            int release = Integer.parseInt(arr[4]);
-            String genre = arr[5];
-           Song out = new Song(name, artist, album, duration, release, genre);
-           System.out.println(String.format("%-20s %-20s %-40s %-15d %-15d %-10s", name, artist,album, duration,release, genre));
-
+        Playlist playlist = new Playlist();
+        Scanner inF = new Scanner(new File("spotify_unique_years_artists.txt"));
+        System.out.println("Title\t\t\t\t\t\t Artist\t\t\t\t\tAlbum\t\t\t\t\t\t \t\tYear\t\t\tGenre");
+        while(inF.hasNextLine()){
+            playlist.readSong(inF.nextLine());
         }
         inF.close();
+        Scanner scan = new Scanner(System.in);
+        int option = 0;
+        while(option != 7){
+            System.out.println("====== Spotify Menu ======");
+            System.out.println("1. Sort By Artist (A-Z)");
+            System.out.println("2. Sort By Artist (Z-A)");
+            System.out.println("3. Sort By Year (oldest - newest)");
+            System.out.println("4. Sort By Year (newest - oldest)");
+            System.out.println("5. Sort By Genre");
+            System.out.println("6. Display all songs");
+            System.out.println("7. Quit");
+            System.out.println("Enter choice (1-7): ");
+
+
+        }
+        System.out.println("Enter a genre to search for:");
+        String genre = scan.nextLine();
+        System.out.print("Result: ");
+        playlist.searchSong(genre);
+        scan.close();
 
     }
-}
-
 }
